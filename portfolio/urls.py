4c181edf0bb843d9ecb9a7b222df83b13c07e751
Djang <django.heimgartner@gmail.com>
2021-09-26 21:24:17 +0200

    Install home app for index page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = "Portfolio Admin"
+admin.site.site_title = "Portfolio Admin Portal"
+admin.site.index_title = "Welcome to the Portfolio portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', include('home.urls', namespace='home')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
