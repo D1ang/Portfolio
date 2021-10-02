1f90d8543a71f797e678979985764e6549eb3582
Djang <django.heimgartner@gmail.com>
2021-10-02 08:24:36 +0200

    Change home index to get projects from backend

--- home/views.py
+++ home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from projects.models import ProjectItem
 
 
 def index(request):
     """
     A view that displays the index page.
     """
+    projects = ProjectItem.objects.all()
 
-    return render(request, 'home/index.html')
+    context = {'projects': projects}
+
+    return render(request, 'home/index.html', context)
