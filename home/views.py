ae163b5ea06a181e00b6313695ce60e817bdeca0
Djang <django.heimgartner@gmail.com>
2021-10-03 11:21:27 +0200

    Add video backend to homepage carousel

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from projects.models import ProjectItem
+from videos.models import VideoItem
 
 
 def index(request):
@@ -7,7 +8,11 @@ def index(request):
     A view that displays the index page.
     """
     projects = ProjectItem.objects.all()
+    videos = VideoItem.objects.all()
 
-    context = {'projects': projects}
+    context = {
+        'projects': projects,
+        'videos': videos,
+    }
 
     return render(request, 'home/index.html', context)
