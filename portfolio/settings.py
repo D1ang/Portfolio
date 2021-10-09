34fb2884324a39a93017537259bffd6e415da0f6
Djang <django.heimgartner@gmail.com>
2021-10-09 21:29:17 +0200

    Add domain to allowed host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'heydjang.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'heydjang.herokuapp.com', 'heydjang.com']
 
 
 # Application definition
