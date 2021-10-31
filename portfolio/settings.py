865cba78d28b019d8081bf7e8de70ff6796a0a27
Djang <django.heimgartner@gmail.com>
2021-10-31 16:35:41 +0100

    Set staticfiles storage on non dev

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -143,6 +143,7 @@ if 'DEVELOPMENT' in os.environ:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 else : 
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
