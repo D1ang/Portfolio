5d3de19194390ae87a5c330876ecbb1dbf372fbd
Django Heimgartner <d.heimgartner@tudelft.nl>
2024-09-08 23:51:57 +0200

    Cleanup & rename portfolio to main

---
+++ main/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+admin.site.site_header = 'Portfolio Admin'
+admin.site.site_title = 'Portfolio Admin Portal'
+admin.site.index_title = 'Welcome to the Portfolio portal'
+
+urlpatterns = [
+    path('dashboard/', admin.site.urls),
+    path('', include('home.urls', namespace='home')),
+]
+
+if settings.DEBUG:
+    urlpatterns += path('__debug__/', include('debug_toolbar.urls')),
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
