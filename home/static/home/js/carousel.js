6da9c05e10a78a1e959a05aae8bae4fae65c52d1
Django Heimgartner <d.heimgartner@tudelft.nl>
2024-09-11 00:02:46 +0200

    Refactor front-end as app based

--- home/static/home/js/carousel.js
+++ home/static/home/js/carousel.js
@@ -1,7 +1,7 @@
 $('.responsive').slick({
   dots: true,
-	prevArrow: $('.prev'),
-	nextArrow: $('.next'),
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
   infinite: false,
   speed: 300,
   slidesToShow: 4,
